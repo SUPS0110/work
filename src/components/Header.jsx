82b393a064ee304c85de0396176314b72b3a9227
SUPS0110 <suprima0110@gmail.com>
2022-10-17 21:07:14 +0545

    react try

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/header.css";
+const Header = () => {
+  return <div className="header">
+    <h1>BTS</h1>
+    <h2> Bangtan Sonyeondan </h2>
+    <p>BTS also known as the Bangtan Boys, is a South Korean boy band formed in 2010 and debuting in 2013 under Big Hit Entertainment.
+         The septet—consisting of members Jin, Suga, J-Hope, RM, Jimin, V, and Jungkook—co-writes and co-produces much of their own material.
+          Originally a hip hop group, their musical style has evolved to incorporate a wide range of genres; 
+          their lyrics have often discussed mental health, the troubles of school-age youth and coming of age, loss,
+           the journey towards self-love, and individualism. Their work also frequently references literature,
+            philosophy and psychological concepts, and includes an alternate universe storyline.</p>
+    </div>;
+};
+
+export default Header;
